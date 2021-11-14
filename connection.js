f4d1823833dacdfe4b58944b045f0e80ca978360
Arindam <arinzonenth@gmail.com>
2021-11-14 23:07:14 +0530

    add postgress cred

--- connection.js
+++ connection.js
@@ -1,12 +1,14 @@
 const { Client } = require('pg');
 
-const client = new Client({
+const client = process.env.DATABASE_URL
+/*const client = new Client({
   user: "postgres",
   host: "localhost",
   database: "postgres",
   password: "system",
   port: 5432,
 })
+*/
 client.connect(function(err) {
     if (err) throw err;
     console.log("Connected!");

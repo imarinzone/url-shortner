200231d88d1cf9fed7424b931e077319a2d95081
Arindam <arinzonenth@gmail.com>
2021-11-14 23:22:28 +0530

    update postgres cred

--- connection.js
+++ connection.js
@@ -1,6 +1,10 @@
 const { Client } = require('pg');
 
-const client = process.env.DATABASE_URL
+var connectionString = process.env.DATABASE_URL
+const client = new Client({
+    connectionString: connectionString,
+    ssl: true
+});
 /*const client = new Client({
   user: "postgres",
   host: "localhost",
